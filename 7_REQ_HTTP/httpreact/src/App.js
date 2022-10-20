fa560fc4ba0e79acf61be9ecbc93ab1ff80b9a73
Milena Mota <80923819+eummilena@users.noreply.github.com>
2022-10-20 11:56:30 -0300

    Update App.js

--- 7_REQ_HTTP/httpreact/src/App.js
+++ 7_REQ_HTTP/httpreact/src/App.js
@@ -5,9 +5,26 @@ const url = "http://localhost:3000/products";
 
 function App() {
   const [products, setProducts] = useState([]);
+  // 1 - resgatando dados
+
+  useEffect(async () => {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    setProducts(data);
+  }, []);
+
+  console.log(products);
   return (
     <div className="App">
       <h1>Lista de Produtos</h1>
+      <ul>
+        {
+          products.map((product) => (
+            <li key={product.id}>{product.name} - R${product.price}</li>
+          ))
+        }
+      </ul>
     </div>
   );
 }

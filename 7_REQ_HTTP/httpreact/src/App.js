3cb998055d61824dd59d4f86fcc445ace2007471
Milena Mota <80923819+eummilena@users.noreply.github.com>
2022-10-20 14:08:10 -0300

    Update App.js

--- 7_REQ_HTTP/httpreact/src/App.js
+++ 7_REQ_HTTP/httpreact/src/App.js
@@ -28,37 +28,37 @@ function App() {
     };
     const res = await fetch(url, {
       method: "POST",
-      headers {
-      "Content-type": "application/json"
-    },
+      headers: {
+        "Content-type": "application/json"
+      },
       body: JSON.stringify(product),
     });
-}
-return (
-  <div className="App">
-    <h1>Lista de Produtos</h1>
-    <ul>
-      {
-        products.map((product) => (
-          <li key={product.id}>{product.name} - R${product.price}</li>
-        ))
-      }
-    </ul>
-    <div className="add-product">
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="nome">
-          Nome:
-          <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-        </label>
-        <label htmlFor="preço">
-          Preço:
-          <input type="number" value={price} name="price" onChange={(e) => setPrice(e.target.value)} />
-        </label>
-        <input type="submit" value={Criar} />
-      </form>
+  }
+  return (
+    <div className="App">
+      <h1>Lista de Produtos</h1>
+      <ul>
+        {
+          products.map((product) => (
+            <li key={product.id}>{product.name} - R${product.price}</li>
+          ))
+        }
+      </ul>
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="nome">
+            Nome:
+            <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+          </label>
+          <label htmlFor="preço">
+            Preço:
+            <input type="number" value={price} name="price" onChange={(e) => setPrice(e.target.value)} />
+          </label>
+          <input type="submit" value={Criar} />
+        </form>
+      </div>
     </div>
-  </div>
-);
+  );
 }
 
 export default App;

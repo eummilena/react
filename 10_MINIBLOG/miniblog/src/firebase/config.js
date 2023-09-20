d449675e36bece05926a2ea96c48d8f10ae30872
Milena <milenab_m@outlook.com>
2023-09-20 19:12:40 -0300

    Create config.js

---
+++ 10_MINIBLOG/miniblog/src/firebase/config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD_FHFNYgauiaF9bPoLTGd9ZbIg6omh1aQ",
+  authDomain: "miniblog-27f96.firebaseapp.com",
+  projectId: "miniblog-27f96",
+  storageBucket: "miniblog-27f96.appspot.com",
+  messagingSenderId: "550513857794",
+  appId: "1:550513857794:web:e4c0c41e19c4a036401e1b"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export {db};

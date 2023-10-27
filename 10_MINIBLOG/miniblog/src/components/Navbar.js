aae735108fd986af22e8e4e8ceb7b38fec87c773
Milena Mota <80923819+eummilena@users.noreply.github.com>
2023-10-27 14:13:20 -0300

    up

--- 10_MINIBLOG/miniblog/src/components/Navbar.js
+++ 10_MINIBLOG/miniblog/src/components/Navbar.js
@@ -1,7 +1,13 @@
 import { NavLink } from "react-router-dom"
 import styles from "./Navbar.module.css"
+import { useAuthentication } from "../hooks/useAuthentication"
+import { useAuthValue } from "../context/AuthContext"
 
 const Navbar = () => {
+    const { user } = useAuthValue();
+
+
+
     return (
         <nav className={styles.navbar}>
             <NavLink to="/" className={styles.brand}>
@@ -13,16 +19,34 @@ const Navbar = () => {
                         Home
                     </NavLink>
                 </li>
-                <li>
-                    <NavLink to="/login" className={({ isActive }) => (isActive ? styles.active : "")}>
-                        Entrar
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/register" className={({ isActive }) => (isActive ? styles.active : "")}>
-                        Cadastrar
-                    </NavLink>
-                </li>
+                {!user &&
+                    <>
+                        <li>
+                            <NavLink to="/login" className={({ isActive }) => (isActive ? styles.active : "")}>
+                                Entrar
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/register" className={({ isActive }) => (isActive ? styles.active : "")}>
+                                Cadastrar
+                            </NavLink>
+                        </li>
+                    </>}
+                {user && (
+                    <>
+                        <li>
+                            <NavLink to="/posts/create" className={({ isActive }) => (isActive ? styles.active : "")}>
+                                Novo post
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/dashboard" className={({ isActive }) => (isActive ? styles.active : "")}>
+                                Dashboard
+                            </NavLink>
+                        </li>
+                    </>
+
+                )}
                 <li>
                     <NavLink to="/about" className={({ isActive }) => (isActive ? styles.active : "")}>
                         Sobre
--- 10_MINIBLOG/miniblog/src/pages/Register/Register.js
+++ 10_MINIBLOG/miniblog/src/pages/Register/Register.js
@@ -1,18 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import styles from './Register.module.css'
+import { useAuthentication } from '../../hooks/useAuthentication';
 
 const Register = () => {
+    const [displayName, setDisplayName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setconfirmPassword] = useState("");
+    const [error, setError] = useState("");
+    const { createUser, error: authError, loading } = useAuthentication();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setError("");
+
+        const user = {
+            displayName,
+            email,
+            password
+        }
+        if (password !== confirmPassword) {
+            setError("As senhas precisam ser iguais!");
+            return
+        }
+        const res = await createUser(user);
+        console.log(res);
+    };
+
+    useEffect(() => {
+        if (authError) {
+            setError(authError);
+        }
+    }, [authError]);
     return (
-        <div>
+        <div className={styles.register}>
             <h1>Cadastre-se para postar</h1>
             <p>Crie seu usuário e compartilhe suas histórias</p>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>
                     <span>Nome:</span>
                     <input
                         type='text'
                         name='displayName'
                         required
-                        placeholder='Nome do usuário' />
+                        placeholder='Nome do usuário'
+                        value={displayName}
+                        onChange={(e) => setDisplayName(e.target.value)}
+                    />
+
                 </label>
                 <label>
                     <span>E-mail:</span>
@@ -20,7 +55,9 @@ const Register = () => {
                         type='email'
                         name='email'
                         required
-                        placeholder='E-mail do usuário' />
+                        placeholder='E-mail do usuário'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)} />
                 </label>
                 <label>
                     <span>Senha:</span>
@@ -28,7 +65,9 @@ const Register = () => {
                         type='password'
                         name='password'
                         required
-                        placeholder='Insira sua senha' />
+                        placeholder='Insira sua senha'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)} />
                 </label>
                 <label>
                     <span>Confirme sua senha:</span>
@@ -36,9 +75,15 @@ const Register = () => {
                         type='password'
                         name='ConfirmPassword'
                         required
-                        placeholder='Confirme sua senha' />
+                        placeholder='Confirme sua senha'
+                        value={confirmPassword}
+                        onChange={(e) => setconfirmPassword(e.target.value)}
+                    />
                 </label>
-                <button className='btn'>Cadastrar</button>
+                {!loading && <button className='btn'>Cadastrar</button>}
+                {loading && <button className='btn' disabled>Aguarde...</button>}
+
+                {error && <p className='error'>{error}</p>}
             </form>
         </div>
     )

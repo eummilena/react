4e6923ecd8f94bdd5ce6431e2081d30f7ac12cad
Milena Mota <80923819+eummilena@users.noreply.github.com>
2023-10-30 11:05:02 -0300

    up

--- 10_MINIBLOG/miniblog/src/components/Navbar.js
+++ 10_MINIBLOG/miniblog/src/components/Navbar.js
@@ -5,6 +5,7 @@ import { useAuthValue } from "../context/AuthContext"
 
 const Navbar = () => {
     const { user } = useAuthValue();
+    const { logout } = useAuthentication();
 
 
 
@@ -52,6 +53,11 @@ const Navbar = () => {
                         Sobre
                     </NavLink>
                 </li>
+                {user && (
+                    <li>
+                        <button onClick={logout}>Sair</button>
+                    </li>
+                )}
             </ul>
         </nav>
     )

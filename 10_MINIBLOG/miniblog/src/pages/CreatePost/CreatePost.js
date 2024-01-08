8e7c803224287bfd54b270de022636dcd273cb72
Milena Mota <80923819+eummilena@users.noreply.github.com>
2024-01-08 11:02:51 -0300

    up

--- 10_MINIBLOG/miniblog/src/pages/CreatePost/CreatePost.js
+++ 10_MINIBLOG/miniblog/src/pages/CreatePost/CreatePost.js
@@ -12,43 +12,68 @@ const CreatePost = () => {
     const [formError, setFormError] = useState("");
 
     const { user } = useAuthValue();
+
+    const navigate = useNavigate();
+
     const { insertDocument, response } = useInsertDocument("posts");
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        setFormError("")
+        setFormError("");
 
-        //validate image  URL
+        // validate image
+        try {
+            new URL(image);
+        } catch (error) {
+            setFormError("A imagem precisa ser uma URL.");
+        }
 
-        //criar o array de tags
+        // create tags array
+        const tagsArray = tags.split(",").map((tag) => tag.trim().toLowerCase());
 
-        //checar todos os valores
+        // check values
+        if (!title || !image || !tags || !body) {
+            setFormError("Por favor, preencha todos os campos!");
+        }
+
+        console.log(tagsArray);
+
+        console.log({
+            title,
+            image,
+            body,
+            tags: tagsArray,
+            uid: user.uid,
+            createdBy: user.displayName,
+        });
+
+        if (formError) return
 
         insertDocument({
             title,
             image,
             body,
-            tags,
+            tags: tagsArray,
             uid: user.uid,
-            createBy: user.displayName
+            createdBy: user.displayName,
         });
 
-        //REDIRECT TO HOME PAGE 
-
+        // redirect to home page
+        navigate("/");
     };
 
     return (
         <div className={styles.create_post}>
             <h2>Criar post</h2>
-            <p>Escreva o que quiser e compartilhe o seu conhecimento</p>
+            <p>Escreva sobre o que quiser e compartilhe o seu conhecimento!</p>
             <form onSubmit={handleSubmit}>
                 <label>
                     <span>Título:</span>
                     <input
-                        type='text'
-                        name='title'
+                        type="text"
+                        name="text"
                         required
-                        placeholder='Pense em um bom título...'
+                        placeholder="Pense num bom título..."
                         onChange={(e) => setTitle(e.target.value)}
                         value={title}
                     />
@@ -56,42 +81,47 @@ const CreatePost = () => {
                 <label>
                     <span>URL da imagem:</span>
                     <input
-                        type='text'
-                        name='image'
+                        type="text"
+                        name="image"
                         required
-                        placeholder='Insira uma imagem que representa o seu post...'
+                        placeholder="Insira uma imagem que representa seu post"
                         onChange={(e) => setImage(e.target.value)}
                         value={image}
                     />
                 </label>
                 <label>
                     <span>Conteúdo:</span>
-                    <textarea name='body'
+                    <textarea
+                        name="body"
                         required
-                        placeholder='Insira o conteúdo do post'
+                        placeholder="Insira o conteúdo do post"
                         onChange={(e) => setBody(e.target.value)}
-                        value={body}>
-                    </textarea>
+                        value={body}
+                    ></textarea>
                 </label>
                 <label>
                     <span>Tags:</span>
                     <input
-                        type='text'
-                        name='tags'
+                        type="text"
+                        name="tags"
                         required
-                        placeholder='Insira as tags separadas por vírgula...'
+                        placeholder="Insira as tags separadas por vírgula"
                         onChange={(e) => setTags(e.target.value)}
                         value={tags}
                     />
                 </label>
-
-                {!response.loading && <button className="btn">Cadastrar</button>}
-                {response.loading && (<button className="btn" disabled>Aguarde...</button>)}
-
-                {response.error && <p className="error">{response.error}</p>}
+                {!response.loading && <button className="btn">Criar post!</button>}
+                {response.loading && (
+                    <button className="btn" disabled>
+                        Aguarde.. .
+                    </button>
+                )}
+                {(response.error || formError) && (
+                    <p className="error">{response.error || formError}</p>
+                )}
             </form>
         </div>
-    )
-}
+    );
+};
 
-export default CreatePost
+export default CreatePost;
